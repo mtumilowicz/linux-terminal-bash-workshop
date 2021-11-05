3a7548f0dfbd9811453476c2ddb3ce20e8c9983d
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-11-05 21:49:43 +0100

    script.sh

--- script.sh
+++ script.sh
@@ -3,16 +3,32 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+trap CLEANUP 0 EXIT
+
 DIR_COUNT=3
 FILE_COUNT=3
+PARENT_DIR=$(pwd)
+script_logging_level="DEBUG"
+
+declare -A levels=([DEBUG]=0 [ERROR]=1)
+
+LOG() {
+  local log_priority=$1
+  local log_message=$2
+
+  if [[ ${levels[$log_priority]} -gt ${levels[$script_logging_level]} ]]
+  then
+    echo "${log_priority} : ${log_message}"
+  fi
+}
 
 LOG_RESULT () {
   local COMMAND_DESCRIPTION=$1
   if [[ $? -eq 0 ]]
   then
-    echo "Success when $COMMAND_DESCRIPTION"
+    LOG "DEBUG" "Success when $COMMAND_DESCRIPTION"
   else
-    echo "Failure when $COMMAND_DESCRIPTION"
+    LOG "ERROR" "Failure when $COMMAND_DESCRIPTION"
   fi
 }
 
@@ -42,28 +58,28 @@ VERIFY_CREATED_FILE_COUNT() {
   EXPECTED_CREATED_FILE_COUNT=$(($DIR_COUNT * FILE_COUNT))
   if [[ $CREATED_FILE_COUNT -eq EXPECTED_CREATED_FILE_COUNT ]]
   then
-    echo "Number of created files is correct: $EXPECTED_CREATED_FILE_COUNT"
+    LOG "DEBUG" "Number of created files is correct: $EXPECTED_CREATED_FILE_COUNT"
   else
-    echo "Number of created files is incorrect: $CREATED_FILE_COUNT and should be: $EXPECTED_CREATED_FILE_COUNT"
+    echo "ERROR" "Number of created files is incorrect: $CREATED_FILE_COUNT and should be: $EXPECTED_CREATED_FILE_COUNT"
   fi
 }
 
 CLEANUP() {
-  cd ..
+  cd $PARENT_DIR
   rm -r workspace
 }
 
-echo "Create workspace"
+LOG "DEBUG" "Create workspace"
 mkdir workspace
 LOG_RESULT "creating Workspace/"
 
 cd workspace
 
-echo "Creating $DIR_COUNT directories"
+LOG "DEBUG" "Creating $DIR_COUNT directories"
 DO_N_TIMES $DIR_COUNT "CREATE_DIR directory"
 LOG_RESULT "creating directories"
 
-echo "Create $FILE_COUNT files per each directory"
+LOG "DEBUG" "Create $FILE_COUNT files per each directory"
 for DIR in */
 do
   cd $DIR
@@ -76,13 +92,11 @@ LOG_RESULT "creating files in directories"
 
 VERIFY_CREATED_FILE_COUNT
 
-echo "Hiding needle.txt file somewhere"
+LOG "DEBUG" "Hiding needle.txt file somewhere"
 RANDOM_DIR=$(shuf --input-range 1-$DIR_COUNT --head-count 1)
 touch directory"$RANDOM_DIR"/needle.txt
 LOG_RESULT "hiding needle"
 
-echo "Moving to the parent parent dir"
+LOG "DEBUG" "Moving to the parent parent dir"
 find . -type f -name "needle.txt" -exec mv {} .. \;
 LOG_RESULT "moving needle to the Workspace/"
-
-trap CLEANUP 0 EXIT
